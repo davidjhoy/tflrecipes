f067e735c6be9d43cdeacbef7916efd1a7f0adcd
david <davidjhoyjr@gmail.com>
2023-04-10 23:21:19 -0400

    secret

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,40 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { BaseSyntheticEvent } from 'react';
+import React, { useState } from "react";
 
 export default function Home() {
+  const router = useRouter();
+  const [wiggle, setWiggle] = useState(false);
+
+  function handleSubmit(e: BaseSyntheticEvent){
+   
+    e.preventDefault();
+    const secret= e.target[0].value;
+
+    if(secret == "raisins"){
+      router.push('/recipes');
+    }else{
+      setWiggle(true);
+    }
+
+
+    
+  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <h2 className = "text-3xl">
-          TFL Recipes
-        </h2>
-        <div>
-        <button>
-          +
-        </button>
+    <main className="flex min-h-screen flex-col">
+
+
+      <div className = "mt-[30vh]">
+        <form className="flex flex-col items-center space-y-6 tracking-widest" onSubmit={handleSubmit}>
+          <div className = "text-3xl"> Fistfuls of:</div>
+          <input type="text" name="secret" className={`${wiggle ?  "border-rose-500" : ""} bg-black border border-1px rounded-md h-10 tracking-wider pl-3 focus:hover:active:enabled:border-sky-200 focus:outline-none`} onChange = {()=> setWiggle(false)}/>
+          <input type="submit" value="Submit" className = "text-lg active:font-medium"/>
+        </form>
       </div>
-      </div>
+      
      
       
     </main>
---
+++ app/recipes/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Recipes(){
+
+
+    return(
+        <div>
+            Recipes
+        </div>
+    )
+}
